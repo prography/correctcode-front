aaf38a9730b7c3529267a4fca9407709ff513bb5
illlama <beatlemaniakih@gmail.com>
2019-11-13 16:00:56 +0900

    prettier 적용

--- src/store/auth/index.ts
+++ src/store/auth/index.ts
@@ -4,43 +4,43 @@ import { User } from 'models/user';
 import AuthAction, { LoginAction, MeAction } from './action';
 
 export type AuthState = {
-	user: User & {
-		isLoggedIn: boolean;
-	};
-	login: {
-		status: Status;
-	};
-	me: {
-		status: Status;
-	};
+  user: User & {
+    isLoggedIn: boolean;
+  };
+  login: {
+    status: Status;
+  };
+  me: {
+    status: Status;
+  };
 };
 
 const initialState: AuthState = {
-	user: {
-		email: '',
-		isLoggedIn: false
-	},
-	login: {
-		status: 'INIT'
-	},
-	me: {
-		status: 'INIT'
-	}
+  user: {
+    email: '',
+    isLoggedIn: false,
+  },
+  login: {
+    status: 'INIT',
+  },
+  me: {
+    status: 'INIT',
+  },
 };
 
 const reducer = createReducer<AuthAction, AuthState>(initialState, {
-	...baseAsyncActionHandler('login', LoginAction),
-	...baseAsyncActionHandler('me', MeAction),
-	[MeAction.success]: (state, action) => {
-		return produce(state, (draft) => {
-			draft.me.status = 'SUCCESS';
-			draft.user.isLoggedIn = true;
-			draft.user = {
-				...action.payload,
-				isLoggedIn: true
-			};
-		});
-	}
+  ...baseAsyncActionHandler('login', LoginAction),
+  ...baseAsyncActionHandler('me', MeAction),
+  [MeAction.success]: (state, action) => {
+    return produce(state, draft => {
+      draft.me.status = 'SUCCESS';
+      draft.user.isLoggedIn = true;
+      draft.user = {
+        ...action.payload,
+        isLoggedIn: true,
+      };
+    });
+  },
 });
 
 export default reducer;
