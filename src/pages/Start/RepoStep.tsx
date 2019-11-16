4ec219e760d81261e09ebc81aa3a239b717cd298
Godsenal <tmqps78@gmail.com>
2019-11-17 01:20:22 +0900

    레포 검색기능 추가

--- src/pages/Start/RepoStep.tsx
+++ src/pages/Start/RepoStep.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState, useMemo } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import classnames from 'classnames';
 import { getReposSaga } from 'store/repo/action';
@@ -9,9 +9,18 @@ import profileImg from 'assets/img/TemporaryProfileImg.png';
 import styles from 'scss/RepoStep.module.scss';
 
 const RepoStep = () => {
+  const [searchWord, setSearchWord] = useState('');
   const username = useSelector((state: StoreState) => state.auth.user.name);
   const repos = useSelector((state: StoreState) => state.repo.repos);
   const dispatch = useDispatch();
+
+  const handleSearchWordChange = (e: React.ChangeEvent<HTMLInputElement>) =>
+    setSearchWord(e.target.value);
+
+  const repoResults = useMemo(
+    () => repos.filter(({ name }) => name.match(searchWord)),
+    [repos, searchWord],
+  );
 
   useEffect(() => {
     dispatch(getReposSaga());
@@ -26,11 +35,16 @@ const RepoStep = () => {
         </div>
         <div className={styles.search}>
           <i className={classnames('fas fa-search', styles.icon)}></i>
-          <input className={styles.input} placeholder="Search Repo" />
+          <input
+            className={styles.input}
+            placeholder="Search Repo"
+            value={searchWord}
+            onChange={handleSearchWordChange}
+          />
         </div>
       </div>
       <div className={styles.repoListBody}>
-        {repos.map(repo => (
+        {repoResults.map(repo => (
           <RepoItem key={repo.id} {...repo} />
         ))}
       </div>
