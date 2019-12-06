b3564a67afd6005cdfecca6dd76ee2cf01fe398f
Godsenal <tmqps78@gmail.com>
2019-12-07 00:33:54 +0900

    리뷰 생성 추가

--- src/pages/Start/ReviewStep.tsx
+++ src/pages/Start/ReviewStep.tsx
@@ -1,12 +1,14 @@
 import React, { memo, useState, useMemo, useEffect } from 'react';
-import { useParams } from 'react-router';
-import { useSelector } from 'react-redux';
+import { useParams, useHistory } from 'react-router';
+import { useSelector, useDispatch } from 'react-redux';
 import classnames from 'classnames';
 import { Dropdown } from 'components';
 import GithubIcon from 'assets/img/GitHubMark.png';
 import useFetch from 'hooks/useFetch';
 import { getBranches } from 'api/repo';
 import { Repo } from 'models/repo';
+import { createReviewSaga } from 'store/review/action';
+import usePrevious from 'hooks/usePrevious';
 
 import styles from 'scss/pages/ReviewStep.module.scss';
 
@@ -17,18 +19,25 @@ type Props = {
 };
 
 const ReviewStep: React.FC<Props> = () => {
+  const dispatch = useDispatch();
+  const history = useHistory();
   const { repoId } = useParams();
   const [firstBranch, setFirstBranch] = useState('');
   const [secondBranch, setSecondBranch] = useState('');
   // const [tag, setTag] = useState('');
   const [message, setMessage] = useState('');
-  const currentRepo = useSelector((state: StoreState) =>
-    state.repo.repos.find(({ id }) => String(id) === repoId),
+  const currentRepo =
+    useSelector((state: StoreState) =>
+      state.repo.repos.find(({ id }) => String(id) === repoId),
+    ) || ({} as any);
+  const createReviewStatus = useSelector(
+    (state: StoreState) => state.review.createReviewStatus,
   );
+  const prevStatus = usePrevious(createReviewStatus);
 
   const isMaxMessageCount = message.length === MAX_MESSAGE_COUNT;
   const isButtonActive = message && firstBranch && secondBranch;
-  const { name = 'aa/ff' } = currentRepo || ({} as any);
+  const { name = '' } = currentRepo;
   const [ownername, reponame] = name.split('/');
 
   const onFirstBranchSelect = (branch: string) => setFirstBranch(branch);
@@ -36,6 +45,17 @@ const ReviewStep: React.FC<Props> = () => {
   // const onTagSelect = (tag: string) => setTag(tag);
   const handleMessageChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
     setMessage(e.target.value);
+  };
+  const handleCreateReview = () => {
+    repoId &&
+      dispatch(
+        createReviewSaga(repoId, {
+          title: 'temp title',
+          description: message,
+          base: firstBranch,
+          head: secondBranch,
+        }),
+      );
   };
 
   // branch 패칭
@@ -64,6 +84,16 @@ const ReviewStep: React.FC<Props> = () => {
       setMessage(prev => prev.slice(0, MAX_MESSAGE_COUNT));
     }
   }, [message]);
+
+  useEffect(() => {
+    if (prevStatus === 'FETCHING' && createReviewStatus === 'SUCCESS') {
+      history.push('/reviewee');
+    }
+  }, [createReviewStatus, prevStatus, history]);
+
+  if (!repoId) {
+    return null;
+  }
 
   return (
     <div className={styles.container}>
@@ -123,7 +153,11 @@ const ReviewStep: React.FC<Props> = () => {
                 onSelect={onTagSelect}
               />
             </div> */}
-            <button className={styles.button} disabled={!isButtonActive}>
+            <button
+              className={styles.button}
+              disabled={!isButtonActive}
+              onClick={handleCreateReview}
+            >
               등록 완료하기
             </button>
           </div>
