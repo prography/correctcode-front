527c77bc4164786c108e664c86861a47c0e80198
Godsenal <tmqps78@gmail.com>
2019-12-21 00:48:15 +0900

    로딩 lottie 추가

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Lottie } from 'components';
 import styles from 'scss/components/Loading.module.scss';
-import animationData from 'assets/img/lottie/data.json';
+import animationData from 'assets/img/lottie/loading.json';
 
 const Loading: React.FC = () => {
   return (
     <div className={styles.container}>
-      <Lottie animationData={animationData} containerStyle={{ height: 120 }} />
+      <Lottie animationData={animationData} containerStyle={{ height: 60 }} />
     </div>
   );
 };
