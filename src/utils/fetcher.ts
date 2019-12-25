52fdc1ad179114191b7ea9945f7550963d94e348
Godsenal <tmqps78@gmail.com>
2019-12-25 23:13:48 +0900

    token 제거

--- src/utils/fetcher.ts
+++ src/utils/fetcher.ts
@@ -1,16 +1,7 @@
 import axios from 'axios';
-import { getAuthToken } from './auth';
 
 const instance = axios.create({
   baseURL: '/api',
-});
-
-instance.interceptors.request.use(config => {
-  const token = getAuthToken();
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-  return config;
 });
 
 export default instance;
