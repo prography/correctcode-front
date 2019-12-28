748e0dc85767794771579541ad15c822abd603d9
godsenal <tmqps78@gmail.com>
2019-12-28 13:19:21 +0900

    me api에 auth token 추가

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,12 +1,12 @@
 import fetcher from 'utils/fetcher';
 import { User } from 'models/user';
 
-export const login = async (token?: string) => {
-  const headers = token ? { Authorization: `Bearer ${token}` } : {};
-  await fetcher.get('/auth/github', { headers });
+export const login = async () => {
+  await fetcher.get('/auth/github');
 };
 
-export const me = async () => {
-  const { data } = await fetcher.get<User>('/me');
+export const me = async (token?: string) => {
+  const headers = token ? { Authorization: `Bearer ${token}` } : {};
+  const { data } = await fetcher.get<User>('/me', { headers });
   return data;
 };
